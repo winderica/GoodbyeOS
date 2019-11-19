a17f7d87492b4105a71bb5c3e652290be6daefb5
winderica <winderica@gmail.com>
2019-11-19 13:31:20 +0800

    Update: minor fixes && finish

--- 3/main.cpp
+++ 3/main.cpp
@@ -31,6 +31,7 @@ class Semaphore {
     int sem;
 public:
     Semaphore(int id, int number) {
+        // create semaphore in constructor
         sem = semget(getKey(id), number, 0666 | IPC_CREAT);
         if (sem < 0) {
             cerr << "Failed to create semaphore." << endl;
@@ -39,6 +40,7 @@ public:
     }
 
     ~Semaphore() {
+        // destroy semaphore in destructor
         if (semctl(sem, 0, IPC_RMID, 0) < 0) {
             cerr << "Failed to destroy semaphore." << endl;
             exit(EXIT_FAILURE);
@@ -46,23 +48,20 @@ public:
     }
 
     void set(int number, int value) {
+        // set value of semaphore
         if (semctl(sem, number, SETVAL, value)) {
             cerr << "Failed to set semaphore value." << endl;
             exit(EXIT_FAILURE);
         }
     }
 
-
     void P(unsigned short index) const {
-        sembuf option = {index, -1, 0};
-        semop(sem, &option, 1);
+        semop(sem, new sembuf{index, -1, 0}, 1);
     }
 
     void V(unsigned short index) const {
-        sembuf option = {index, 1, 0};
-        semop(sem, &option, 1);
+        semop(sem, new sembuf{index, 1, 0}, 1);
     }
-
 };
 
 template<typename T>
@@ -87,7 +86,7 @@ void destroySharedMemory(int shm) {
     }
 }
 
-void readProcess(const Semaphore &s, int shmID, char* filename) {
+void readProcess(const Semaphore &s, int shmID, char *filename) {
     auto file = fopen(filename, "rb");
     if (file == nullptr) {
         cerr << "Failed to open file." << endl;
@@ -95,24 +94,25 @@ void readProcess(const Semaphore &s, int shmID, char* filename) {
     }
     while (true) {
         s.P(1);
-        s.P(2);
-        auto readBuffer = getSharedMemory<Buffer>(shmID);
-        auto size = fread(readBuffer->data, 1, BUFFER_SIZE, file);
-        readBuffer->size = size;
-        if (!size) {
-            readBuffer->end = true;
+        s.P(2); // mutex
+        auto buffer = getSharedMemory<Buffer>(shmID);
+        // `fread` is used here rather than `ifstream`
+        auto size = fread(buffer->data, 1, BUFFER_SIZE, file);
+        buffer->size = size;
+        if (!size) { // EOF
+            buffer->end = true;
             fclose(file);
             s.V(2);
             s.V(0);
             return;
         }
-        shmID = readBuffer->next;
+        shmID = buffer->next;
         s.V(2);
         s.V(0);
     }
 }
 
-void writeProcess(const Semaphore &s, int shmID, char*filename) {
+void writeProcess(const Semaphore &s, int shmID, char *filename) {
     auto file = fopen(filename, "wb");
     if (file == nullptr) {
         cerr << "Failed to open file." << endl;
@@ -120,15 +120,17 @@ void writeProcess(const Semaphore &s, int shmID, char*filename) {
     }
     while (true) {
         s.P(0);
-        s.P(3);
-        auto writeBuffer = getSharedMemory<Buffer>(shmID);
-        fwrite(writeBuffer->data, writeBuffer->size, 1, file);
-        if (writeBuffer->end) {
+        s.P(3); // mutex
+        auto buffer = getSharedMemory<Buffer>(shmID);
+        // `fwrite` is used here rather than `ofstream`
+        fwrite(buffer->data, buffer->size, 1, file);
+        if (buffer->end) {
+            fclose(file);
             s.V(3);
             s.V(1);
             return;
         }
-        shmID = writeBuffer->next;
+        shmID = buffer->next;
         s.V(3);
         s.V(1);
     }
@@ -136,18 +138,18 @@ void writeProcess(const Semaphore &s, int shmID, char*filename) {
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <input file> <output file>", argv[0]);
+        cout << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
         exit(EXIT_FAILURE);
     }
 
-    // create semaphore
+    // create semaphore && set values
     Semaphore semaphore(ID, 4);
-    // set value
     semaphore.set(0, 0);
     semaphore.set(1, BUFFER_NUMBER);
     semaphore.set(2, 1);
     semaphore.set(3, 1);
 
+    // create && initialize shared memories
     int shmTemp = createSharedMemory<Buffer>(0);
     int shmID = shmTemp;
     Buffer *buffer;
@@ -155,33 +157,34 @@ int main(int argc, char *argv[]) {
         buffer = getSharedMemory<Buffer>(shmTemp);
         shmTemp = createSharedMemory<Buffer>(i);
         buffer->size = 0;
-        buffer->next = shmTemp;
+        buffer->next = shmTemp; // pointer to next
         buffer->end = false;
     }
     buffer = getSharedMemory<Buffer>(shmTemp);
     buffer->size = 0;
-    buffer->next = shmID;
+    buffer->next = shmID; // pointer to head
     buffer->end = false;
-    pid_t readPID = fork();
-    if (readPID == -1) { // failed to fork read process
+
+    pid_t readPID = fork(); // fork read process
+    if (readPID == -1) {
         cout << "Failed to fork read process." << endl;
         exit(EXIT_FAILURE);
     } else if (readPID == 0) { // read process
         readProcess(semaphore, shmID, argv[1]);
         exit(EXIT_SUCCESS);
     } else {
-        pid_t writePID = fork();
-        if (writePID == -1) { // failed to fork child write process
+        pid_t writePID = fork(); // fork write process
+        if (writePID == -1) {
             cout << "Failed to fork write process." << endl;
             exit(EXIT_FAILURE);
         } else if (writePID == 0) { // write process
             writeProcess(semaphore, shmID, argv[2]);
             exit(EXIT_SUCCESS);
-        } else {
+        } else { // main process
             waitpid(readPID, nullptr, 0);
             waitpid(writePID, nullptr, 0);
             do {
-                destroySharedMemory(buffer->next);
+                destroySharedMemory(buffer->next); // destroy shared memory
                 buffer = getSharedMemory<Buffer>(buffer->next);
             } while (buffer->next != shmID);
             // semaphore is destroyed in its destructor
--- 4/main.cpp
+++ 4/main.cpp
@@ -15,21 +15,28 @@ using namespace filesystem;
 
 auto recursive = false;
 
-auto getTime(file_time_type time) {
-    using namespace chrono;
+auto getPermissions(perms p) {
     stringstream ss;
-    auto t = system_clock::to_time_t(time_point_cast<system_clock::duration>(time - file_time_type::clock::now() + system_clock::now()));
-    ss << BLUE << put_time(localtime(&t), "%Y %b %d %H:%M");
+    ss << YELLOW << ((p & perms::owner_read) != perms::none ? "r" : "-")    // owner readable
+       << RED << ((p & perms::owner_write) != perms::none ? "w" : "-")      // owner writable
+       << GREEN << ((p & perms::owner_exec) != perms::none ? "x" : "-")     // owner executable
+       << YELLOW << ((p & perms::group_read) != perms::none ? "r" : "-")    // group readable
+       << RED << ((p & perms::group_write) != perms::none ? "w" : "-")      // group writable
+       << GREEN << ((p & perms::group_exec) != perms::none ? "x" : "-")     // group executable
+       << YELLOW << ((p & perms::others_read) != perms::none ? "r" : "-")   // others readable
+       << RED << ((p & perms::others_write) != perms::none ? "w" : "-")     // others writable
+       << GREEN << ((p & perms::others_exec) != perms::none ? "x" : "-");   // others executable
     return ss.str();
 }
 
 auto getSize(optional<double> originalSize) {
     stringstream ss;
-    ss << CYAN << setw(3);
+    ss << CYAN << setw(4);
     if (originalSize == nullopt) {
-        ss << " " << "-";
+        ss << " " << "-"; // trick for alignment
     } else {
         double size = originalSize.value();
+        // convert size into human-friendly format
         ss << fixed;
         if (size < KB) {
             ss << setprecision(0) << size << "B";
@@ -52,50 +59,46 @@ auto getSize(optional<double> originalSize) {
 
 auto getUserAndGroup(const struct stat &info) {
     stringstream ss;
-    ss << YELLOW << setw(10) << getpwuid(info.st_uid)->pw_name << " "
-       << BRIGHT_YELLOW << setw(10) << getgrgid(info.st_gid)->gr_name;
+    auto pwUID = getpwuid(info.st_uid);
+    auto grGID = getgrgid(info.st_gid);
+    ss << YELLOW << setw(10) << (pwUID == nullptr ? to_string(info.st_uid) : pwUID->pw_name) << " " // username
+       << BRIGHT_YELLOW << setw(10) << (grGID == nullptr ? to_string(info.st_gid) : grGID->gr_name); // group name
+    return ss.str();
+}
+
+auto getTime(file_time_type time) {
+    using namespace chrono;
+    stringstream ss;
+    auto t = system_clock::to_time_t(time - file_time_type::clock::now() + system_clock::now()); // convert time
+    ss << BLUE << put_time(localtime(&t), "%Y %b %d %H:%M"); // format time
     return ss.str();
 }
 
 auto getFilename(const directory_iterator &entry) {
     stringstream ss;
     auto path = entry->path();
-    if (entry->is_directory()) {
-        ss << MAGENTA << path.filename().string() << RESET_COLOR;
-    } else {
-        ss << RESET_COLOR << entry->path().filename().string();
-    }
+    ss << (entry->is_directory() ? MAGENTA : RESET_COLOR) << path.filename().string();
     if (entry->is_symlink()) {
-        ss << BRIGHT_CYAN << " -> " << read_symlink(path).string() << RESET_COLOR;
+        // show original path of symbolic link
+        ss << BRIGHT_CYAN << " -> " << read_symlink(path).string();
     }
+    ss << RESET_COLOR;
     return ss.str();
 }
 
-auto getPermissions(perms p) {
-    stringstream ss;
-    ss << YELLOW << ((p & perms::owner_read) != perms::none ? "r" : "-")
-       << RED << ((p & perms::owner_write) != perms::none ? "w" : "-")
-       << GREEN << ((p & perms::owner_exec) != perms::none ? "x" : "-")
-       << YELLOW << ((p & perms::group_read) != perms::none ? "r" : "-")
-       << RED << ((p & perms::group_write) != perms::none ? "w" : "-")
-       << GREEN << ((p & perms::group_exec) != perms::none ? "x" : "-")
-       << YELLOW << ((p & perms::others_read) != perms::none ? "r" : "-")
-       << RED << ((p & perms::others_write) != perms::none ? "w" : "-")
-       << GREEN << ((p & perms::others_exec) != perms::none ? "x" : "-")
-       << RESET_COLOR;
-    return ss.str();
-}
-
+// implemented by C++17 `filesystem` STL
 void ls(const string &path, const string &prefix = "") {
     auto entry = directory_iterator(path);
-    auto emptyEntry = directory_iterator();
+    auto emptyEntry = end(entry);
     while (entry != emptyEntry) {
         stringstream ss;
-        struct stat info{};
         auto nextPath = entry->path().string();
-        stat(nextPath.c_str(), &info);
         auto isDirectory = entry->is_directory();
         auto isSymLink = entry->is_symlink();
+        // `stat` is used because `filesystem` has no APIs related to user and group
+        struct stat info{};
+        stat(nextPath.c_str(), &info);
+        // ascii colors are used to print colors
         ss << BRIGHT_BLUE << (isSymLink ? "l" : isDirectory ? "d" : ".")
            << getPermissions(entry->status().permissions()) << " "
            << BRIGHT_MAGENTA << setw(3) << entry->hard_link_count() << " "
@@ -104,26 +107,26 @@ void ls(const string &path, const string &prefix = "") {
            << getTime(entry->last_write_time()) << " "
            << getFilename(entry) << endl;
         entry++;
-        cout << prefix << (entry == emptyEntry ? "└─" : "├─") << ss.str();
+        // box drawing characters are used to print result like tree
+        cout << BRIGHT_BLACK << prefix << (entry == emptyEntry ? "└─" : "├─") << ss.str();
         if (recursive && isDirectory) {
             ls(nextPath, prefix + (entry == emptyEntry ? " " : "│") + " ");
         }
     }
 }
 
+// implemented by native C APIs
 void lss(const string &path, const string &prefix = "") {
     string realPath = realpath(path.c_str(), nullptr);
     realPath += "/";
     chdir(realPath.c_str());
     auto dir = opendir(realPath.c_str());
     if (dir == nullptr) {
-        cout << "Failed to get directory info!" << endl;
-        return;
+        throw exception();
     }
 
     vector<dirent *> entries;
     dirent *temp;
-
     while ((temp = readdir(dir))) {
         string name = temp->d_name;
         if (name != "." && name != "..") {
@@ -132,40 +135,25 @@ void lss(const string &path, const string &prefix = "") {
     }
 
     for (auto entry: entries) {
-        string name = entry->d_name;
+        auto name = entry->d_name;
         struct stat info{};
-        stringstream ss;
-        stat(name.c_str(), &info);
-        auto perm = info.st_mode;
+        stat(name, &info);
         auto isDirectory = entry->d_type == DT_DIR;
         auto isSymLink = entry->d_type == DT_LNK;
-        ss << BRIGHT_BLUE << (isSymLink ? "l" : isDirectory ? "d" : ".")
-           << YELLOW << (perm & 0400 ? "r" : "-")
-           << RED << (perm & 0200 ? "w" : "-")
-           << GREEN << (perm & 0100 ? "x" : "-")
-           << YELLOW << (perm & 040 ? "r" : "-")
-           << RED << (perm & 020 ? "w" : "-")
-           << GREEN << (perm & 010 ? "x" : "-")
-           << YELLOW << (perm & 04 ? "r" : "-")
-           << RED << (perm & 02 ? "w" : "-")
-           << GREEN << (perm & 01 ? "x" : "-")
-           << RESET_COLOR << " "
-           << BRIGHT_MAGENTA << setw(3) << info.st_nlink << " "
-           << getSize((isSymLink || isDirectory) ? nullopt : optional<double>(info.st_size)) << " "
-           << getUserAndGroup(info) << " "
-           << BLUE << put_time(localtime(&(info.st_mtime)), "%Y %b %d %H:%M ");
-        if (isDirectory) {
-            ss << MAGENTA << name << RESET_COLOR;
-        } else {
-            ss << RESET_COLOR << name;
-        }
-        if (isSymLink) {
-            ss << BRIGHT_CYAN << " -> " << read_symlink(realPath + name).string() << RESET_COLOR;
-        }
-        ss << endl;
-        cout << prefix << (entry == entries.back() ? "└─" : "├─") << ss.str();
+        auto fullPath = realPath + name;
+        cout << prefix
+             << (entry == entries.back() ? "└─" : "├─")
+             << BRIGHT_BLUE << (isSymLink ? "l" : isDirectory ? "d" : ".")
+             << getPermissions(static_cast<perms>(info.st_mode)) << " "
+             << BRIGHT_MAGENTA << setw(3) << info.st_nlink << " "
+             << getSize((isSymLink || isDirectory) ? nullopt : optional<double>(info.st_size)) << " "
+             << getUserAndGroup(info) << " "
+             << BLUE << put_time(localtime(&(info.st_mtime)), "%Y %b %d %H:%M ")
+             << (isDirectory ? MAGENTA : RESET_COLOR) << name
+             << BRIGHT_CYAN << (isSymLink ? " -> " + read_symlink(fullPath).string() : "")
+             << RESET_COLOR << endl;
         if (recursive && isDirectory) {
-            lss(realPath + name, prefix + (entry == entries.back() ? " " : "│") + " ");
+            lss(fullPath, prefix + (entry == entries.back() ? " " : "│") + " ");
             chdir(realPath.c_str());
         }
     }
@@ -186,7 +174,7 @@ int main(int argc, char **argv) {
         if (paths.empty()) {
             ls(".");
         } else {
-            for (const auto& path: paths) {
+            for (const auto &path: paths) {
                 ls(path);
             }
         }
